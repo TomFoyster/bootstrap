1622d3622c8b50b6c084f8fe1d3a404a2dab400c
TomFoyster <TFOYSTER@next-uk.next.loc>
2017-03-07 10:11:01 +0000

    Broke down script tasks

---
+++ build/tasks/scripts.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+// This loads all plugins from our project dependencies into the $ variable.
+// No need to include them all here.
+var $ = require('gulp-load-plugins')();
+var fs = require('fs');
+var paths = require('../paths');
+
+var pkg = JSON.parse(fs.readFileSync('./package.json'));
+var babelConfig = {
+  "presets": [
+    [
+      "es2015",
+      {
+        "modules": false,
+        "loose": true
+      }
+    ]
+  ],
+  "plugins": [
+    "transform-es2015-modules-strip"
+  ]
+};
+
+
+// Code to insertBefore
+var banner = '/*!\n' +
+          ' * Bootstrap v' + pkg.version + ' (' + pkg.homepage +')\n' +
+          ' * Copyright 2011-' + new Date().getFullYear() + ' ' + pkg.author + '\n' +
+          ' * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)\n' +
+          ' */\n';
+var jqueryCheck = 'if (typeof jQuery === \'undefined\') {\n' +
+               '  throw new Error(\'Bootstrap\\\'s JavaScript requires jQuery. jQuery must be included before Bootstrap\\\'s JavaScript.\')\n' +
+               '}\n';
+var jqueryVersionCheck = '+function ($) {\n' +
+                      '  var version = $.fn.jquery.split(\' \')[0].split(\'.\')\n' +
+                      '  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] >= 4)) {\n' +
+                      '    throw new Error(\'Bootstrap\\\'s JavaScript requires at least jQuery v1.9.1 but less than v4.0.0\')\n' +
+                      '  }\n' +
+                      '}(jQuery);\n\n';
+
+
+var bannerInsert = banner + '\n' + jqueryCheck + '\n' + jqueryVersionCheck + '\n+function () {\n';
+var footerInsert = '\n}();';
+
+
+gulp.task('scripts-build-src', function() {
+  return gulp.src(['js/dist/util.js', 'js/dist/alert.js', 'js/dist/button.js', 'js/dist/carousel.js', 'js/dist/collapse.js', 'js/dist/dropdown.js', 'js/dist/modal.js', 'js/dist/scrollspy.js', 'js/dist/tab.js', 'js/dist/tooltip.js', 'js/dist/popover.js'])
+    .pipe($.sourcemaps.init())
+    .pipe($.babel({extends: '../../js/.babelrc'}))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('js/src'));
+})
+
+gulp.task('scripts-build-dist', function() {
+  return gulp.src(['js/src/util.js', 'js/src/alert.js', 'js/src/button.js', 'js/src/carousel.js', 'js/src/collapse.js', 'js/src/dropdown.js', 'js/src/modal.js', 'js/src/scrollspy.js', 'js/src/tab.js', 'js/src/tooltip.js', 'js/src/popover.js'])
+    .pipe($.concat('./bootstrap.js'))
+    .pipe($.insert.wrap(bannerInsert, footerInsert))
+    .pipe(gulp.dest(paths.jsOut));
+})
+
+gulp.task('scripts-build', function(callback) {
+  return runSequence(
+    'scripts-build-src',
+    /*'scripts-build-dist',*/
+    callback
+  )
+})
