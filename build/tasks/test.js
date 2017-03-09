e534c89b9a2000b4f6c300afc3015ac5a8f8ecb2
TomFoyster <TFOYSTER@next-uk.next.loc>
2017-03-09 11:18:58 +0000

    Tweaked how run tasks are ran; Added qunit tests

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -16,14 +16,19 @@ gulp.task('test', function(callback) {
 
 gulp.task('test-styles', function() {
   return gulp.src(paths.scssSource)
-    .pipe(scsslint({
+    .pipe($.scssLint({
         'config': paths.scssLintConfig
     }))
 });
 
-gulp.task('test-scripts', function() {
-  /*return gulp.src(['**//*.js','!node_modules/**'])
-       .pipe(eslint())
-       .pipe(eslint.format())
-       .pipe(eslint.failAfterError());*/
+gulp.task('test-scripts', function(callback) {
+  return runSequence(
+    'test-scripts-qunit',
+    callback
+  );
 });
+
+gulp.task('test-scripts-qunit', function() {
+  return gulp.src('./js/tests/index.html')
+        .pipe($.qunit());
+})
